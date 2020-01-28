a585f8ba618f4619e42b8099b9b29d49c2e7eb73
Ryan-ops <57469976+Ryan-ops@users.noreply.github.com>
2020-01-27 19:09:48 -0500

    Add characters sketch

---
+++ characters/sketch.js
@@ -0,0 +1,16 @@
+/* 
+characters sketch
+1.27.2020
+
+*/
+
+// run once to setup the browser
+function setup(){
+	createCanvas(windowWidth, windowHeight);
+
+}
+
+// runs 60fps draws graphics
+function draw(){
+	background ('lightblue');
+}
