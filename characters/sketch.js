33088964136fba1e7d10c7fe7a62f619a36d9d2a
Ryan-ops <57469976+Ryan-ops@users.noreply.github.com>
2020-01-29 19:20:29 -0500

    add character

--- characters/sketch.js
+++ characters/sketch.js
@@ -3,14 +3,47 @@ characters sketch
 1.27.2020
 
 */
+// loading graphics
+var jak;
+var daxter;
+/*
+runs before setup
+when preload is done,
+setup gets called 
+*/
+function preload(){
+	jak = loadImage('Jak.png');
+	daxter = loadImage('Daxter.png');
+}
 
+
+// Location variables
+var JakX = 100;
+var JakY = 200;
+
+var DaxterX = 500;
+var DaxterY = 510;
+
+var story = "Once upon a time, there were two characters named Giant and ";
 // run once to setup the browser
 function setup(){
 	createCanvas(windowWidth, windowHeight);
 
 }
 
+
+
 // runs 60fps draws graphics
 function draw(){
 	background ('lightblue');
+
+	// draw characters
+	image(jak, JakX, JakY);
+	image(daxter, DaxterX, DaxterY);
+
+		// narration
+	textSize(30);
+	textAlign(CENTER, CENTER);
+	text(story, width/4, 20, width/2);
 }
+
