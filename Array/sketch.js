192a250bea7ac315861cdc73e689c90ece702daa
Ryan-ops <57469976+Ryan-ops@users.noreply.github.com>
2020-03-04 19:27:57 -0500

    added array

--- Array/sketch.js
+++ Array/sketch.js
@@ -9,73 +9,101 @@ function preload() {
 
 var daxX = [];
 var daxY = [];
-var numDax = 10;
+var numDax = 7;
+
+var daxterX = [];
+var daxterY = [];
+var numDaxter = 7;
+var daxterSpeedX = [];
+var daxterSpeedY = [];
+
+
+var jakX = [];
+var jakY = [];
+var numJak = 1;
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
-    setting();
+  
     
     //add dax positions
-    let x = -50;
+    let x = 350;
     for (let i = 0; i < numDax; i++){
         daxX.push(x);
         //update x, distributing number of clouds across canvas
         x += width/numDax + random(-100, 100);
         daxY.push(random(height/2));
     }
+    
+    let y = 750;
+    for(let i = 0;  i < numJak; i++){
+        jakX.push(random(jakImage.width, width - jakImage.width));
+        jakY.push(y);
+        y+=30;
+    
+    
+    
+    }
+    
+    for (let i = 0; i < numDaxter; i++){
+        daxterX.push(random(width));
+        daxterY.push(height -daxterImage.height - random(100));
+        daxterSpeedX.push(random(1,3));
+        daxterSpeedY.push(random(-2,2));
+        
+        
+    
+    
+    }
 }
 
 
 
 function draw(){
-    for(let i = 0; i < numDax; i++){
-        image(daxImage, daxX[i], daxY[i]);
-        
-        //animate x
-        daxX[i] -= 1;
-        
-        //check if  is beyond right side of cavas
-        if (daxX[i] > width){
-            //reset cloud back to left side
-            daxX[i] = -daxImage.width;
-        }
-
-    }
-
-}
-
-function mousePressed() {
-	setting();
-}
-
-
-function setting() {
-	// background
-	
-    
     imageMode(CENTER);
-		image(parkImage, width/2, height * 2/3);
-
-	// images
-	
-
-for (let x = -50; x <= width; x += 200) {
-		let y = random(height/2);
-		let offset = random(-90, 150);
-		image(daxterImage, x + offset, y);
-}
-
-
-	
-	for (let x = 100; x <= width; x += 400) {
-		image(jakImage, random(width), height/2 - 150 + x/20);}
+  image(parkImage, width/2 , height * 2/3);
     
+    //Jak
+    for (let i =0; i < numJak; i++){
+        image(jakImage, jakX[i], jakY[i]);
+    
+    
+    }
+    
+    //Daxter
+      for (let i = 0; i < numDaxter; i++) {
+		image(daxterImage, daxterX[i], daxterY[i]);
 
-	// 
-	for (let x = -50; x <= width; x += 100) {
-		image(daxImage, x, height - random(100));
+		// animate
+		daxterX[i] += daxterSpeedX[i] + random(1);
+		daxterY[i] += daxterSpeedY[i] + random(-0.5, 0.5);
 
+		// reset daxter
+		if (daxterX[i] > width) {
+			daxterX[i] = -daxterImage.width;
+		}
 
+		// contain y value of daxter
+		if (daxterY[i] < height * 2/3 || 
+			daxterY[i] > height - daxterImage.height) {
+			daxterSpeedY[i] *= -1;
+		}
 	}
+        //dax
+        
+        for (let i = 0; i < numDax; i++) {
+		image(daxImage, daxX[i], daxY[i]);
+
+		// animate x
+		daxX[i] += 1;
+
+		// check if dax is beyond right side of canvas
+		if (daxX[i] > width) {
+			// reset dax back to left side
+			daxX[i] = -daxImage.width;
+		}
+	}
+              
 
 }
+
